4ab46cdef6d6a11996e4ae11237d54e1b1c05fa1
Ashwini Gosavi <120784052+ashwinigosavi27@users.noreply.github.com>
2023-06-16 10:08:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
 //complete this code
-class Person {}
+class Person:
+    def __init__(self, name, age):
+        self._name = name
+        self._age = age
 
-class Student extends Person {}
+    @property
+    def name(self):
+        return self._name
 
-class Teacher extends Person {}
+    @property
+    def age(self):
+        return self._age
 
-// Do not change the code below this line
-window.Person = Person;
-window.Student = Student;
-window.Teacher = Teacher;
+    @age.setter
+    def age(self, value):
+        self._age = value
+
+
+class Student(Person):
+    def study(self):
+        print(f"{self.name} is studying.")
+
+
+class Teacher(Person):
+    def teach(self):
+        print(f"{self.name} is teaching.")
+
+let person = new Person("John", 25);
+console.log(person.name);  // Output: John
+console.log(person.age);  // Output: 25
+
+person.age = 30;
+console.log(person.age);  // Output: 30
+
+let student = new Student("Alice", 20);
+student.study();  // Output: Alice is studying.
+
+let teacher = new Teacher("Mr. Smith", 35);
+teacher.teach();  // Output: Mr. Smith is teaching.
+
